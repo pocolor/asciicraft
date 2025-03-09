9a4c33ff0b13feab9059d44a55e549a0d68e8deb
pocolor <luvecjr@gmail.com>
2025-03-09 21:51:03 +0100

    Permutations added for noise generation

--- src/main/java/pocolor/asciicraft/world/generation/noise/Noise.java
+++ src/main/java/pocolor/asciicraft/world/generation/noise/Noise.java
@@ -1,5 +1,13 @@
 package pocolor.asciicraft.world.generation.noise;
 
+import java.util.Random;
+
 public abstract class Noise {
+    protected final Permutations permutations;
+
+    public Noise(Random random) {
+        this.permutations = new Permutations(8, random);  // FIXME(number of permutations probably shouldn't be a constant)
+    }
+
     public abstract double noise(double x, double y);
 }
---
+++ src/main/java/pocolor/asciicraft/world/generation/noise/Permutations.java
@@ -0,0 +1,41 @@
+package pocolor.asciicraft.world.generation.noise;
+
+import java.util.Random;
+
+public class Permutations {
+    private final int[] permutations;
+    private final int bound;
+    
+    public Permutations(int powerOf2, Random random) {
+        this.permutations = createPermutations(1 << powerOf2, random);
+        this.bound = this.permutations.length - 1;
+    }
+    
+    public int getAt(int index) {
+        return this.permutations[index & this.bound];
+    }
+    
+    private static int[] createPermutations(int length, Random random) {
+        int[] permutations = new int[length];
+        
+        for (int i = 0; i < length; i++) {
+            permutations[i] = i;
+        }
+        
+        shuffle(permutations, random);
+        
+        return permutations;
+    }
+    
+    private static void shuffle(int[] permutations, Random random) {
+        for (int i = permutations.length - 1; i > 0; i--) {
+            swap(permutations, i, random.nextInt(i));
+        }
+    }
+    
+    private static void swap(int[] permutations, int index1, int index2) {
+        int temp = permutations[index1];
+        permutations[index1] = permutations[index2];
+        permutations[index2] = temp;
+    }
+}
